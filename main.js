17b3d107ef4d3e3c9de5a931d8ea369ac87b4615
Elly Post <elly@ellytronic.media>
2017-03-29 12:11:35 -0500

    cleaned up design, mountains now static. removed mg mtn

--- main.js
+++ main.js
@@ -1,43 +1,37 @@
 var WIDTH = 1024;
 var HEIGHT = 512;
+var TILES_WIDE = 56;
 var game, cursors, player, map;
+var bgMtn, mgMtn; // paralax backgrounds
 var layers = {};
+var jumpTimer = 0;
 
 function preload() {  
-    // Here we preload the assets
-    
     game.load.tilemap('map', 'assets/maps/map.json', null, Phaser.Tilemap.TILED_JSON)
-    game.load.image('mountain-bg', 'assets/png/mountain-bg-512x512.png');
+    game.load.image('mountains-bg', 'assets/png/mountains-bg-1024x512.png');
     game.load.image('mountain-mg', 'assets/png/mountain-mg-512x512.png');
     game.load.image('tileset', 'assets/png/tileset.png');
-    game.load.image('tree', 'assets/png/tree-128x128.png');
     game.load.image('player', 'assets/png/cat-sprite.png');
-    game.load.image('sky', 'assets/png/sky.png');
-
     game.load.spritesheet('cat', 'assets/png/cat-sprite.png', 34, 52, 10);
-
 }
 
 function create() {  
-    // Here we create the game
-    //Set the background color to blue
     game.stage.backgroundColor = '#3598db';
-    game.add.tileSprite(0, 0, WIDTH, HEIGHT, 'sky');
 
-    map = game.add.tilemap('map', 32, 32, 56, 16);
+    map = game.add.tilemap('map', 32, 32, TILES_WIDE, 16);
     map.addTilesetImage('tileset', 'tileset');
-    map.addTilesetImage('mountain-bg-512x512', 'mountain-bg');
-    map.addTilesetImage('mountain-mg-512x512', 'mountain-mg');
-    map.addTilesetImage('tree-128x128', 'tree');
 
-    // var bgSky = map.createLayer('background-sky');
-    layers.collision = map.createLayer('collision');
-    layers.bgMtn = map.createLayer('background-mountain');
-    layers.mgMtn = map.createLayer('midground-mountain');
+    // add the sky and resize the world
+    layers.sky = map.createLayer('background-sky');
+    layers.sky.resizeWorld();
+    // now the backgrounds in order of appearance
+    bgMtn = game.add.sprite(0, 0, 'mountains-bg');
+    bgMtn.fixedToCamera = true; // lock the mountains in place
+    // mgMtn = game.add.sprite(game.width/2, 0, 'mountain-mg');    
+    layers.collisions = map.createLayer('collisions');
     layers.ground = map.createLayer('ground');
-    layers.bgTrees = map.createLayer('background-tree');
+    layers.bgTrees = map.createLayer('background-trees');
     layers.bgGrass = map.createLayer('background-grass');
-    layers.collision.resizeWorld();
 
     // add the player
     player = game.add.sprite(10, 350, 'cat');
@@ -46,11 +40,12 @@ function create() {
 
     // Start the Arcade physics system (for movements and collisions)
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.enable(layers.collision, Phaser.Physics.ARCADE);
-    map.setCollisionBetween(0, 2000, true, 'collision');
-    layers.collision.body.immovable = true;
+    game.physics.enable(layers.collisions, Phaser.Physics.ARCADE);
+    map.setCollisionBetween(0, 2000, true, 'collisions');
+    layers.collisions.body.immovable = true;
     game.physics.enable(player, Phaser.Physics.ARCADE);
     player.body.collideWorldBounds = true;
+    player.body.bounce.y = 0.1;
 
     // Variable to store the arrow key pressed
     cursors = game.input.keyboard.createCursorKeys();
@@ -63,14 +58,18 @@ function create() {
 }
 
 function update() {  
-    game.physics.arcade.collide(layers.collision, player);
+    // collision handling
+    game.physics.arcade.collide(layers.collisions, player);
 
+    // movement
     if (cursors.left.isDown) 
     {   
+        // mgMtn.position.x += 0.3;
         walkStart("left");
     }
     else if (cursors.right.isDown) 
     {
+        // mgMtn.position.x -= 0.3;
         walkStart("right");
     }
     else 
@@ -78,23 +77,21 @@ function update() {
         walkStop();
     }
 
-    // if (cursors.up.isDown)
-    // {
-    //     player.loadTexture('mario', 5);   // this loads the frame 5 (jump) of my mario spritesheet
-    //     if(touchingDown(player)){  // this checks if the player is on the floor (we don't allow airjumps)
-    //         player.body.velocity.y = -800;   // change the y velocity to -800 means "jump!"
-    //     }
-    // }
+    if (cursors.up.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    {
+        player.body.velocity.y = -250;
+        jumpTimer = game.time.now + 250;
+    }
 }
 
 function walkStart(dir) {
     if (dir == "right") {
         player.scale.x = 1;
-        player.body.velocity.x = 150;
+        player.body.velocity.x = 200;
         player.animations.play('walk', 10, true);
     } else {
         player.scale.x = -1;
-        player.body.velocity.x = -150;
+        player.body.velocity.x = -200;
         player.animations.play('walk', 10, true);
     }
 }
